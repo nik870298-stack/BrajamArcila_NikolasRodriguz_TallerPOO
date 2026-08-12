65bd811dbe780174b77e964046b65f72669e0fed
Nikolas R <nik870298@gmail.com>
2026-08-12 17:37:35 -0500

    inicio

---
+++ src/Vehiculo.java
@@ -0,0 +1,69 @@
+public class Vehiculo {
+    String placa;
+    String marca;
+    String modelo;
+    int año;
+    double precioBase;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(String placa, String marca, String modelo, int año, double precioBase) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.precioBase = precioBase;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setPrecioBase(double precioBase) {
+        this.precioBase = precioBase;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" +
+                "placa='" + placa + '\'' +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", año=" + año +
+                ", precioBase=" + precioBase +
+                '}';
+    }
+}
